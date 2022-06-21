c4c22c1f302c9e865bbd5ca6da071fc1541a252a
Irfan Adi <irfan.adi1209@gmail.com>
2022-06-21 17:54:00 +0700

    Completed function delete and edit, perfecting style, adding credits under h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class Home extends React.Component {
     super(props);
     this.state = {
         textInput: '',
-        items: []
+        items: [],
+        editInput: ''
     }
   }
 
@@ -23,87 +24,107 @@ class Home extends React.Component {
     });
   }
 
+  // Separating Edit to prevent conflicts in the state
 
+  handleChangeEdit = (event) => {
+    this.setState({
+      editInput: event.target.value
+    });
+  }
 
+  handleClick2Edit = (i) => {
+    this.setState(this.state.items.splice(i, 1, this.state.editInput));
+    this.setState({editInput: ''});
+  }
+
+  handleDelete = (i) => {
+    this.setState(this.state.items.splice(i, 1));
+  }
+
+  
   render(){
-
     return (
-      <div className='rounded' style={{
-        width: '50%',
+      <div>
+        <div className='rounded shadow' style={{
+        maxWidth: '700px',
+        minWidth: '385px',
+        minHeight: '500px',
         marginLeft: 'auto',
         marginRight:'auto',
         marginTop:'10%',
         marginBottom: '50px',
-        backgroundColor:'gray',
+        backgroundColor:'#28559A',
         paddingBottom:'20px'
         }}>
-        <h1 style={{textAlign: 'center', paddingTop:'20px', paddingBottom:'20px'}}>React Todo List</h1>
-        <form onSubmit={this.handleSubmit}
-          className='input-group'
-          style={{
-            width:'80%',
-            marginLeft:'auto',
-            marginRight:'auto',
-            marginBottom:'20px'
-          }}>
-          <input
-            type="text"
-            value={this.state.textInput}
-            onChange={this.handleChange}
-            className='form-control'
+          <h1 style={{textAlign: 'center', paddingTop:'20px', paddingBottom:'20px', color:'white', textShadow:'2px 2px 4px #000000'}}>React Todo List</h1>
+          <p style={{textAlign: 'center', paddingBottom:'10px', color:'white', textShadow:'2px 2px 4px #000000'}}>By: <a rel='noreferrer' href="https://github.com/irfanadi1209" target='_blank' style={{color:'white'}}>Irfan Adi</a></p>
+          <form onSubmit={this.handleSubmit}
+            className='input-group shadow'
             style={{
-              width:'80%'
-            }}
-            placeholder='Masukkan tugas anda disini!'
-          />
-          <button className='btn btn-success' type='submit'>Enter</button>
-        </form>
-        <div className="card-todo">
-          <ul>
-            {
-              this.state.items.map((item, index) => {
-                const idCardModal = `modalCard${index}`;
-                const modalTarget = `#${idCardModal}`;
-                const modalLabel = `${idCardModal}Label`;
+              width:'80%',
+              marginLeft:'auto',
+              marginRight:'auto',
+              marginBottom:'20px'
+            }}>
+            <input
+              type="text"
+              value={this.state.textInput}
+              onChange={this.handleChange}
+              className='form-control'
+              style={{
+                width:'80%'
+              }}
+              placeholder='Masukkan tugas anda disini!'
+            />
+            <button className='btn btn-success' type='submit'>Enter</button>
+          </form>
+          <div className="card-todo">
+            <ul>
+              {
+                this.state.items.map((item, index) => {
+                  const idCardModal = `modalCard${index}`;
+                  const modalTarget = `#${idCardModal}`;
+                  const modalLabel = `${idCardModal}Label`;
 
-                return (
-                <li style={{listStyle:'none'}} key={index}>
-                      {/* Card and Trigger Modal */}
-                      <div style={{marginBottom:'10px', marginRight: '25px', cursor:'pointer'}}>
-                        <div className="col">
-                          <div className="card" data-bs-toggle='modal' data-bs-target={modalTarget}>
-                            <div className="card-body">
-                              <h5 className="card-title">Tugas ke - {index+1}</h5>
-                              <p className="card-text">{item}</p>
-                              <sub>Click for details.</sub>
+                  return (
+                  <li style={{listStyle:'none'}} key={index}>
+                        {/* Card and Trigger Modal */}
+                        <div style={{marginBottom:'10px', marginRight: '25px', cursor:'pointer'}} >
+                          <div className="col">
+                            <div className="card shadow" style={{backgroundColor:'#3778C2', color:'white'}} data-bs-toggle='modal' data-bs-target={modalTarget}>
+                              <div className="card-body ">
+                                <h5 className="card-title">Tugas ke - {index+1}</h5>
+                                <h2 className="card-text">{item}</h2>
+                                <sub>Click for edit.</sub>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                      {/* Modal */}
-                      <div className="modal fade" id={idCardModal} tabindex="-1" aria-labelledby={modalLabel} aria-hidden="true">
-                        <div className="modal-dialog modal-dialog-centered">
-                          <div className="modal-content">
-                            <div className="modal-header">
-                              <h5 className="modal-title" id={modalLabel}>Detail Tugas ke - {index+1}</h5>
-                              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-                              <input type='text' value={item}/>
-                            </div>
-                            <div className="modal-footer">
-                              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                              <button type='button' className='btn btn-danger'>Delete</button>
-                              <button type="button" className="btn btn-primary">Save changes</button>
+                        {/* Modal */}
+                        <div className="modal fade" id={idCardModal} tabindex="-1" aria-labelledby={modalLabel} aria-hidden="true">
+                          <div className="modal-dialog modal-dialog-centered">
+                            <div className="modal-content">
+                              <div className="modal-header">
+                                <h5 className="modal-title" id={modalLabel}>Edit Tugas ke - {index+1}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div className="modal-body">
+                                <input type='text' placeholder={item} onChange={this.handleChangeEdit}/>
+                              </div>
+                              <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type='button' className='btn btn-danger' onClick={()=>{this.handleDelete(index)}} data-bs-dismiss="modal">Delete</button>
+                                <button type="button" className="btn btn-warning" onClick={()=>{this.handleClick2Edit(index)}} data-bs-dismiss="modal">Save changes</button>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                </li>
-                )
-              })
-            }
-          </ul>
+                  </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
         </div>
       </div>
     );
